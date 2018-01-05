b9be38cddd2a6cbe40fa2694928db4de2b1c2ea5
vanshralhan <vansh@maropost.com>
2018-01-05 20:24:11 +0530

    Production File

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,6 @@ Rails.application.configure do
     }
   config.action_mailer.perform_caching = false
 
-    }
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
