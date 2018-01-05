3cca7d83ac6850e5aae33f120806087ac2940677
vanshralhan <vansh@maropost.com>
2018-01-05 13:25:58 +0530

    Added Cloudinary

--- app/uploaders/movie_uploader.rb
+++ app/uploaders/movie_uploader.rb
@@ -3,7 +3,7 @@ class MovieUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+   include Cloudinary::CarrierWave
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -36,9 +36,9 @@ class MovieUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
