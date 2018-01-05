249cedeba87cdd7bcbbdfe820f92e92a2d735bba
vanshralhan <vansh@maropost.com>
2018-01-05 19:21:52 +0530

    Production smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,13 +37,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://moviedetail.herokuapp.com/'}
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
     authentication: 'plain',
-    enable_starttls_auto: true,
+    # enable_starttls_auto: true,
     user_name: 'ralhanvansh02@gmail.com',
     password: '84874705vV'
     }
