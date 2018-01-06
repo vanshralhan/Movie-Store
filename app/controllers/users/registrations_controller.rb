07061ed65dd7e793c09cfcf1c3c4a4334944fe4c
vanshralhan <vansh@maropost.com>
2018-01-06 15:47:36 +0530

    Devise Layout

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  after_create :confirmation_message
   # GET /resource/sign_up
   # def new
   #   super
@@ -57,4 +57,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private
+    def confirmation_message
+      flash[:alert] = "Confirmation Mail Has been sent to your email."
+    end
 end
