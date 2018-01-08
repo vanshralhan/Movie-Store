06a3b5e4d0a7855c3388fc0f344b0f1447a10967
vanshralhan <vansh@maropost.com>
2018-01-08 19:51:07 +0530

    Secrets/Api/devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = Figaro.env.Email
+  config.mailer_sender = Rails.application.secrets.Email
 
 
   # Configure the class responsible to send e-mails.
